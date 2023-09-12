161691e51caddf182782c8958a55465446cfaef4
Ovidiu <63751494+ovidiu123@users.noreply.github.com>
2023-09-12 16:37:18 +0300

    Update ooplab1.java
    updated

--- ooplab1.java
+++ ooplab1.java
@@ -1,24 +1,23 @@
-public class Guitar {
-    private boolean isMoving;
-    private int speed;
-    private String color;
+// hello world
 
-    // Constructor to initialize the guitar's state
-    public Guitar(boolean isMoving, int speed, String color) {
-        this.isMoving = isMoving;
+public class Guitar {
+    boolean isPlaying;
+    int speed;
+    String color;
+
+    public Guitar(boolean isPlaying, int speed, String color) {
+        this.isPlaying = isMoving;
         this.speed = speed;
         this.color = color;
     }
-
-    // Method to print the guitar's state to the console
     public void printState() {
-        System.out.println("Guitar State: " + (isMoving ? "Moving" : "Standing"));
-        System.out.println("Speed: " + speed + " mph");
+        System.out.println("Guitar State: " + (isPlaying ? "Moving" : "Standing"));
+        System.out.println("Speed: " + speed + " bpm");
         System.out.println("Color: " + color);
     }
 
     public static void main(String[] args) {
-        // Create a Guitar object and print its state
+       
         Guitar myGuitar = new Guitar(true, 30, "Red");
         myGuitar.printState();
     }
