7a0a428e111187327060bedf850b11703b095bc3
Ovidiu <63751494+ovidiu123@users.noreply.github.com>
2023-09-12 16:38:13 +0300

    Update ooplab1.java
    finished

--- ooplab1.java
+++ ooplab1.java
@@ -6,12 +6,12 @@ public class Guitar {
     String color;
 
     public Guitar(boolean isPlaying, int speed, String color) {
-        this.isPlaying = isMoving;
+        this.isPlaying = isPlaying;
         this.speed = speed;
         this.color = color;
     }
     public void printState() {
-        System.out.println("Guitar State: " + (isPlaying ? "Moving" : "Standing"));
+        System.out.println("Guitar State: " + (isPlaying ? "Playing" : "Standing"));
         System.out.println("Speed: " + speed + " bpm");
         System.out.println("Color: " + color);
     }
